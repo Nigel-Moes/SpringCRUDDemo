483bc2d7466bd55c97414b55e47e7605eb17073f
Nigel <mail@nigelmoes.nl>
2024-03-20 10:29:22 -0500

    Changed REST properties configuration

--- src/main/java/com/nigel/SpringCRUDDemo/dao/EmployeeRepository.java
+++ src/main/java/com/nigel/SpringCRUDDemo/dao/EmployeeRepository.java
@@ -2,5 +2,8 @@ package com.nigel.SpringCRUDDemo.dao;
 
 import com.nigel.SpringCRUDDemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> { }
